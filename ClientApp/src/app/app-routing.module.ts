5592a85495b84bb638b41fd13034088ceb93e45c
Nebiyu Elias <nebiyuelias1@gmail.com>
2019-02-28 21:35:25 +0300

    Add cards to the home component.

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { AssignInstructorComponent } from './assign-instructor/assign-instructor
 import { CourseOfferingsListComponent } from './course-offerings-list/course-offerings-list.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { ScheduleConfigurationFormComponent } from './schedule-configuration-form/schedule-configuration-form.component';
+import { CurriculumListComponent } from './curriculum-list/curriculum-list.component';
 
 const routes: Routes = [
     {
@@ -25,13 +26,118 @@ const routes: Routes = [
         component: HomeComponent,
         pathMatch: 'full',
     },
-    { path: 'colleges', component: CollegeListComponent },
-    { path: 'departments/new', component: DepartmentFormComponent },
-    { path: 'curriculums/new', component: CurriculumFormComponent },
-    { path: 'courses/new', component: CourseFormComponent },
-    { path: 'buildings/new', component: BuildingFormComponent },
-    { path: 'rooms/new', component: RoomFormComponent },
-    { path: 'instructors/new', component: InstructorFormComponent },
+    {
+        path: 'colleges',
+        data: {
+            breadcrumb: 'Colleges'
+        },
+        children: [
+            {
+                path: '',
+                data: {
+                    breadcrumb: 'List'
+                },
+                component: CollegeListComponent
+            }
+        ]
+    },
+    {
+        path: 'departments',
+        data: {
+            breadcrumb: 'Departments'
+        },
+        children: [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: DepartmentFormComponent
+            }
+        ]
+    },
+    {
+        path: 'curriculums',
+        data: {
+            breadcrumb: 'Curriculums'
+        },
+        children: [
+            {
+                path: '',
+                data: {
+                    breadcrumb: 'List'
+                },
+                component: CurriculumListComponent
+            },
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: CurriculumFormComponent
+            }
+        ]
+    },
+    {
+        path: 'courses',
+        data: {
+            breadcrumb: 'Courses'
+        },
+        children: [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: CourseFormComponent
+            },
+        ]
+    },
+    {
+        path: 'instructors',
+        data: {
+            breadcrumb: 'Instructors'
+        },
+        children:  [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: InstructorFormComponent
+            }
+        ]
+    },
+    {
+        path: 'buildings',
+        data: {
+            breadcrumb: 'Buildings'
+        },
+        children: [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: BuildingFormComponent
+            }
+        ]
+    },
+    {
+        path: 'rooms',
+        data: {
+            breadcrumb: 'Rooms'
+        },
+        children: [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: RoomFormComponent
+            }
+        ]
+    },
     {
         path: 'sections',
         data: {
@@ -70,12 +176,60 @@ const routes: Routes = [
             },
         ]
     },
+    {
+        path: 'courseofferings',
+        data: {
+            breadcrumb: 'Course Offerings'
+        },
+        children: [
+            {
+                path: '',
+                data: {
+                    breadcrumb: 'List'
+                },
+                component: CourseOfferingsListComponent
+            },
+            {
+                path: 'assign/:id',
+                data: {
+                    breadcrumb: 'Assign'
+                },
+                component: AssignInstructorComponent
+            }
+        ]
+    },
+    {
+        path: 'scheduleconfigurations',
+        data: {
+            breadcrumb: 'Schedule Configurations'
+        },
+        children: [
+            {
+                path: 'new',
+                data: {
+                    breadcrumb: 'New'
+                },
+                component: ScheduleConfigurationFormComponent
+            }
+        ]
+    },
+    {
+        path: 'schedules',
+        data: {
+            breadcrumb: 'Schedules'
+        },
+        children: [
+            {
+                path: ':id',
+                data: {
+                    breadcrumb: 'Section Schedule'
+                },
+                component: ScheduleComponent
+            }
+        ]
+    },
     { path: 'years/new', component: AcademicYearFormComponent },
     { path: 'semesters/new', component: AcademicSemesterFormComponent },
-    { path: 'courseofferings/assign/:id', component: AssignInstructorComponent },
-    { path: 'courseofferings', component: CourseOfferingsListComponent },
-    { path: 'schedules/:id', component: ScheduleComponent },
-    { path: 'scheduleconfigurations', component: ScheduleConfigurationFormComponent }
 ];
 
 
