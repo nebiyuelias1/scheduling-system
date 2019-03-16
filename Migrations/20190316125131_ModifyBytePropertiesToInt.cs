88867acdf0c3ae493ca3ccf4afa826106467fad5
Nebiyu Elias <nebiyuelias1@gmail.com>
2019-03-16 15:52:56 +0300

    Modify byte properties to int and migrate changes to db.

---
+++ Migrations/20190316125131_ModifyBytePropertiesToInt.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchedulingSystem.Migrations
+{
+    public partial class ModifyBytePropertiesToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Period",
+                table: "ScheduleEntries",
+                nullable: false,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Duration",
+                table: "ScheduleEntries",
+                nullable: false,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Tutor",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Lecture",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(byte));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Lab",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(byte));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Period",
+                table: "ScheduleEntries",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Duration",
+                table: "ScheduleEntries",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Tutor",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Lecture",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Lab",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
